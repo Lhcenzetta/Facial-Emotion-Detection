a704b72e7516790c254abb6dbbf85db6e6c8468c
lait-zet <lhcenztta18@gmail.com>
2025-11-14 16:40:12 +0100

    add

--- test_u/test_project.py
+++ test_u/test_project.py
@@ -4,16 +4,12 @@ import os
 
 @pytest.fixture
 def load_model():
-    # Get repo root dynamically
     repo_root = os.path.dirname(os.path.dirname(__file__))
 
-    # Relative path inside the repo
     model_path = os.path.join(repo_root, "My_Model/emotion_detection_model.pkl")
 
-    # Load model
     model = joblib.load(model_path)
     return model
 
 def test_model_loading(load_model):
     assert load_model is not None
-    
