25b5dad070b382563428342a4e789e5ef547f707
lait-zet <lhcenztta18@gmail.com>
2025-11-14 15:37:55 +0100

    ok it wok

--- test_u/test_project.py
+++ test_u/test_project.py
@@ -1,12 +1,19 @@
 import joblib
 import pytest
-
+import os
 
 @pytest.fixture
 def load_model():
-    model_path = "/Users/lait-zet/Desktop/Facial-Emotion-Detection/My_Model/emotion_detection_model.pkl"
+    # Get repo root dynamically
+    repo_root = os.path.dirname(os.path.dirname(__file__))
+
+    # Relative path inside the repo
+    model_path = os.path.join(repo_root, "My_Model/emotion_detection_model.pkl")
+
+    # Load model
     model = joblib.load(model_path)
     return model
 
 def test_model_loading(load_model):
-    assert load_model != None
+    assert load_model is not None
+    
